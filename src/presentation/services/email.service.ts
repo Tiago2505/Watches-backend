70f687983c7900dc11e1141031b8338712c62248
Dany Paz <danysantiago2505@gmail.com>
2026-07-29 17:55:08 -0600

    Cambiando trasnporter

--- src/presentation/services/email.service.ts
+++ src/presentation/services/email.service.ts
@@ -15,44 +15,43 @@ interface Attachement {
 }
 
 export class EmailService {
-
   constructor() {}
 
   private transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
       user: envs.MAILER_EMAIL,
       pass: envs.MAILER_SECRET_KEY,
     },
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000,
   });
 
-
   async sendEmail(options: SendMailOptions): Promise<boolean> {
-  const { to, subject, htmlBody, attachements = [] } = options;
+    const { to, subject, htmlBody, attachements = [] } = options;
 
-  try {
+    try {
+      console.log("1. Iniciando envío de correo");
 
-    console.log("1. Iniciando envío de correo");
+      const sentInformation = await this.transporter.sendMail({
+        to,
+        subject,
+        html: htmlBody,
+        attachments: attachements,
+      });
 
-    const sentInformation = await this.transporter.sendMail({
-      to,
-      subject,
-      html: htmlBody,
-      attachments: attachements,
-    });
+      console.log("2. Correo enviado");
+      console.log(sentInformation.messageId);
 
-    console.log("2. Correo enviado");
-    console.log(sentInformation.messageId);
+      return true;
+    } catch (error) {
+      console.error("3. Error enviando correo");
+      console.error(error);
 
-    return true;
-
-  } catch (error) {
-
-    console.error("3. Error enviando correo");
-    console.error(error);
-
-    return false;
+      return false;
+    }
   }
-}
-  
 }
